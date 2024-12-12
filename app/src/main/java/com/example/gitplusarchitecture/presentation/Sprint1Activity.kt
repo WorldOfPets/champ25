4cb37d92abeb0d5546c8345d034ca968406953bc
jojo <polivanov004@gmail.com>
2024-12-12 11:20:51 +0300

    change name acitvity

--- app/src/main/java/com/example/gitplusarchitecture/presentation/Sprint1Activity.kt
+++ app/src/main/java/com/example/gitplusarchitecture/presentation/Sprint1Activity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.gitplusarchitecture.R
 
-class MainActivity2 : AppCompatActivity() {
+class Sprint1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
